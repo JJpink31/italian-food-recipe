6cc12bd8eeae34e6a10049010e788a84870f2658
Jordanka <140741161+JJpink31@users.noreply.github.com>
2025-07-23 16:37:29 -0500

    Made minor tweeks the output

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateRecipe(event) {
 
    let instructionInput = document.querySelector("#user-instructions");
     let prompt= `User instructions: Generate an Italian food recipe about ${instructionInput.value}`;
-    let context=" You are talented chef with a lot of wisdom about Italain cuisine. Please generate an Italian recipe that has both ingredients and cooktime. Each line must have a <br />. Make sure to follow user instructions."
+    let context=" You are talented chef with a lot of wisdom about Italain cuisine. Please generate an Italian recipe that has both ingredients and cooktime. Written in html, each line must have a <br />. Make sure to follow user instructions. Please don't write html```"
     let apikey="f8833caao3caf01e1ffbc8t348acfb03";
     let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
 
